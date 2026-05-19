7901c99423596555752a0c88cc42324e64ec1526
natnichasuankhwan-del <natnicha.suankhwan@g.swu.ac.th>
2026-05-19 15:18:05 +0000

    fix loop bounds, parameter order, and capitalized actions

--- game.php
+++ game.php
@@ -44,7 +44,7 @@ if ( $action === "Test" ) {
     for($h=0; $h<3; $h++) {
         for($c=0; $c<3; $c++) {
             $r = check($c, $h);
-            $output .= "Human = {$names[$h]} Computer = {$names[$cc]} Result = $r\n";
+            $output .= "Human = {$names[$h]} Computer = {$names[$c]} Result = $r\n";
         }
     }
 }
