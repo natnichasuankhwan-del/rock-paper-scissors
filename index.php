1a5d40b5930254a3c964b321095e11bf17ddcead
natnichasuankhwan-del <natnicha.suankhwan@g.swu.ac.th>
2026-05-19 14:58:21 +0000

    fix all bugs and spacing for autograder

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <title>Rock Paper Scissors bde4e71c</title>
 </head>
 <body>
-    <h1>Welcome to Rock Paper Scissors</h1>
+    <h1>Rock Paper Scissors bde4e71c</h1>
     <p>
 <a href="login.php">Please Log In</a>
 </p>
