dc1cdd4d59b1122132c433b3b54d2ccd04e7fa7a
natnichasuankhwan-del <natnicha.suankhwan@g.swu.ac.th>
2026-05-19 14:34:46 +0000

    fix welcome colon

--- game.php
+++ game.php
@@ -56,7 +56,7 @@ if ( $action === "test" ) {
 <body>
 <div class="container">
     <h1>Rock Paper Scissors</h1>
-    <p><b>Welcome</b> <?= htmlentities($who) ?></p>
+    <p><b>Welcome:</b> <?= htmlentities($who) ?></p>
     <form method="GET" action="game.php">
         <input type="hidden" name="who" value="<?= htmlentities($who) ?>">
         <select name="choice">
